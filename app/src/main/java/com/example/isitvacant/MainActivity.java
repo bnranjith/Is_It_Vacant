ce130f9f1c182b6c28e65113bdf67e6f400f4a65
Chiranthanmv <chiranthanmv@gmail.com>
2020-02-17 16:18:04 +0530

    chiranthan committed

--- app/src/main/java/com/example/isitvacant/MainActivity.java
+++ app/src/main/java/com/example/isitvacant/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.isitvacant;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(Home_fragment.newInstance("", "", "",""));
+
+
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            openFragment(Home_fragment.newInstance("", "", "",""));
+                            return true;
+                        case R.id.navigation_search:
+                            openFragment(Search_fragment.newInstance("", "", "",""));
+                            return true;
+                        case R.id.navigation_reservation:
+                            openFragment(Resevation_fragment.newInstance("", "", "",""));
+                            return true;
+                        case R.id.navigation_profile:
+                            openFragment(profile_fragment.newInstance("", "", "",""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
---
+++ app/src/main/java/com/example/isitvacant/profile_fragment.java
@@ -0,0 +1,54 @@
+package com.example.isitvacant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class profile_fragment extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM3 = "param3";
+    private static final String ARG_PARAM4 = "param4";
+
+    private String mParam1;
+    private String mParam2;
+    private String mParam3;
+    private String mParam4;
+    public profile_fragment() {
+
+    }
+
+
+    public static profile_fragment newInstance(String param1, String param2, String param3,String param4) {
+        profile_fragment fragment = new profile_fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM3, param3);
+        args.putString(ARG_PARAM4, param4);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam3 = getArguments().getString(ARG_PARAM3);
+            mParam4 = getArguments().getString(ARG_PARAM4);
+        }
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_profile_fragment, container, false);
+        return view;
+    }
+}
