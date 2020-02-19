cb42e80381034c550f9d9723143859b5df2b3f95
lenovo <astlerussel@gmail.com>
2020-02-19 22:38:02 +0530

    auth done profile done

--- app/src/main/java/com/example/isitvacant/HomeName.java
+++ app/src/main/java/com/example/isitvacant/HomeName.java
@@ -196,7 +196,7 @@ public class HomeName extends AppCompatActivity {
                     userMap.put("Mobile", mob0);
                     userMap.put("Nationality",nationality );
                     userMap.put("uid",uid2);
-                    userMap.put("image","https://firebasestorage.googleapis.com/v0/b/is-it-vacant.appspot.com/o/profile%20images%2Fprofile_image.png?alt=media&token=fe715eff-284d-477a-a6bc-f15d424ee642" );
+                    userMap.put("image","https://firebasestorage.googleapis.com/v0/b/is-it-vacant-d1cf7.appspot.com/o/profile%20images%2Fprofile_image.png?alt=media&token=07a82599-e485-4e7f-b937-dac00b1ea41d" );
 
 
 
