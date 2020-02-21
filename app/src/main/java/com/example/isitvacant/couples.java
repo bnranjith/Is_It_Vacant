1fbf8b44634e7bd81aa96fdc66175f44a03a25f3
lenovo <astlerussel@gmail.com>
2020-02-21 14:16:30 +0530

    auth done profile done

--- app/src/main/java/com/example/isitvacant/couples.java
+++ app/src/main/java/com/example/isitvacant/couples.java
@@ -1,14 +1,107 @@
 package com.example.isitvacant;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class couples extends AppCompatActivity {
+    private RecyclerView couplesRecyclerList;
+    EditText searchText;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference contactsRef = db.collection("restaurants");
+    private RestaurantsAdapter adapter;
+    Query query;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_couples);
+        couplesRecyclerList = (RecyclerView) findViewById(R.id.couples_recycler);
+        couplesRecyclerList.setLayoutManager(new LinearLayoutManager(this));
+        searchText= findViewById(R.id.search);
+        query = contactsRef.orderBy("name").whereEqualTo("category","couples");
+        setUpRecyclerView(query);
+        searchText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                query = contactsRef.orderBy("name").whereEqualTo("category","couples");
+                setUpRecyclerView(query);
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length()==0){
+                    query = contactsRef.orderBy("name").whereEqualTo("category","couples");
+                    setUpRecyclerView(query);
+                    adapter.startListening();
+
+                }
+                else {
+                    query = contactsRef.orderBy("name").whereEqualTo("category","couples").startAt(s.toString()).endAt(s.toString() + "\uf8ff");
+                    setUpRecyclerView(query);
+                    adapter.startListening();
+                }
+
+
+
+
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+
+
+
+
+
+
+
+            }
+        });
+    }
+    private void setUpRecyclerView(Query query) {
+
+
+
+
+
+
+
+        FirestoreRecyclerOptions<RestaurantsModel> options = new FirestoreRecyclerOptions.Builder<RestaurantsModel>()
+                .setQuery(query, RestaurantsModel.class)
+                .build();
+
+        adapter = new RestaurantsAdapter(options);
+
+        RecyclerView recyclerView = findViewById(R.id.couples_recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+
+        if(FirebaseAuth.getInstance().getCurrentUser()!=null){
+            adapter.startListening();
+
+        }
+
     }
 }
